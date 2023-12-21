20aef5b6a3d42140997ec3ad17fa45b608e1bc63
AKD <contact.mousom@gmail.com>
2023-12-22 03:29:49 +0600

    Login and register page design done

--- src/Routes/PublicRoute/Router.jsx
+++ src/Routes/PublicRoute/Router.jsx
@@ -2,21 +2,35 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../../LayOuts/MainLayout";
 import Home from "../../Pages/Home/Home";
 import AboutUs from "../../Pages/AboutUs/AboutUs";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
 
 const Router = createBrowserRouter([
     {
         path: '/',
-        element: <MainLayout/>,
+        element: <MainLayout />,
         children: [
             {
                 index: true,
-                element: <Home/>
+                element: <Home />
             },
             {
                 path: 'about',
-                element: <AboutUs/>
-            }
+                element: <AboutUs />
+            },
+
         ]
+    },
+
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+
+        path: '/register',
+        element: <Register/>
+
     }
 ])
 
