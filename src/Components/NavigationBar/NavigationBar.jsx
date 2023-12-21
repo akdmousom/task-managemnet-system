2c21957422cf6edaa5d4f425b8ec9d8adb25538c
AKD <contact.mousom@gmail.com>
2023-12-21 21:20:47 +0600

    Navigation bar is added

---
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,35 @@
+const NavigationBar = () => {
+    const navItem = <>
+
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+
+                        {navItem}
+
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navItem}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    );
+};
+
+export default NavigationBar;
--- src/LayOuts/MainLayout.jsx
+++ src/LayOuts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavigationBar from "../Components/NavigationBar/NavigationBar";
 
 const MainLayout = () => {
     return (
-        <div>
-            <h1>Main</h1>
+        <div className="max-w-[1367px] mx-auto">
+            {/* Navigation bar added here */}
+            <NavigationBar/>
             <Outlet/>
         </div>
     );
